e6aae3f8307e484c7e7907436352e5422136f3e1
HyperPig <vitalityhyperpig@gmail.com>
2021-08-12 22:45:22 +0300

    Initial commit

---
+++ app/src/main/java/vitality/uberapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package vitality.uberapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.editText);
+        TextView plusText = findViewById(R.id.plusText);
+        Button plusButton = findViewById(R.id.plusButton);
+        plusText.setMovementMethod(new ScrollingMovementMethod());
+
+        plusButton.setOnClickListener(view -> {
+            if (editText.getText().length() == 0) {
+                editText.setHint("Вы не ввели текст!");
+            } else {
+                editText.setHint("Введите текст");
+                plusText.append("- " + editText.getText().toString() + "\n");
+                editText.setText(null);
+            }
+
+        });
+    }
+}
